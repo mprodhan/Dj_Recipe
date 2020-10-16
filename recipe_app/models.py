33c3c7051f7f25bcb23b3737eae4817332f8327c
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-10-15 23:34:18 -0400

    made migrations.

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=30)
-    bio = models.TextField
+    bio = models.TextField()
 
     def __str__(self):
         return self.name
 
-class Recipe(mdoels.Model):
+class Recipe(models.Model):
     title = models.CharField(max_length=20)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.TextField()
