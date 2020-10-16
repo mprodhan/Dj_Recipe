1f8cbc43d6eb006b35d529a61833276662777c9a
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-10-15 23:19:15 -0400

    created the models for the recipes and the author information.

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    bio = models.TextField
+
+    def __str__(self):
+        return self.name
+
+class Recipe(mdoels.Model):
+    title = models.CharField(max_length=20)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    instructions = models.TextField()
+    time_required = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='img', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.author}"
