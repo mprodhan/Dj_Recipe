f6584791476ef1e15fe69c82629101d1e517ea74
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-10-15 23:49:11 -0400

    added some strings with instructions for views.py.

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+""" 
+1. Start with a general data display for author and recipe.
+2. Include both a recipes add and author add.
+3. Include an author and recipe view.
+  """
